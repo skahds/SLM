fee2be415f61e25a011a0da387a3e828d909c765
skahds <skahd.dev@gmail.com>
2024-09-16 20:17:31 +0700

    turned debug mode off, oops

--- slm/shared/game_start.lua
+++ slm/shared/game_start.lua
@@ -1,5 +1,5 @@
 slm = {
-    debugMode = true
+    debugMode = false
 }
 
 
@@ -9,7 +9,7 @@ umg.on("@playerJoin", function(clientID)
     lp.forceSpawnSlot(lp.main.getContext():getPlot():getPPos(7,9), server.entities.shop_slot,clientID)
 
     if slm.debugMode == true then
-    lp.forceSpawnSlot(lp.main.getContext():getPlot():getPPos(12,12), server.entities.turn_slot,clientID)
+    -- lp.forceSpawnSlot(lp.main.getContext():getPlot():getPPos(12,12), server.entities.turn_slot,clientID)
     lp.forceSpawnItem(lp.main.getContext():getPlot():getPPos(9,8), server.entities.torch,clientID)
     lp.forceSpawnItem(lp.main.getContext():getPlot():getPPos(6,7), server.entities.quartz_glove,clientID)
     lp.forceSpawnItem(lp.main.getContext():getPlot():getPPos(7,7), server.entities.ruby_axe,clientID)
